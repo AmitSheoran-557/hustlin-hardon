d61cccf52885543de27961b9bc063362402a0cf7
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2025-03-27 10:50:01 +0530

    change

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from 'react'
 import { FAQS_LIST } from '@/utils/helper';
+import CustomHeading from './common/CustomHeading';
 
 const Faqs = () => {
     const [active, setActive] = useState(0);
@@ -8,22 +9,25 @@ const Faqs = () => {
         setActive(active === index ? null : index);
     };
     return (
-        <div id='faq' className='px-4'>
-            <div className="container pt-[210px] max-xl:pt-[170px] max-lg:pt-14 max-md:pt-10">
-                <div className="max-w-[1022px] mt-[60px] max-lg:mt-10 max-md:mt-7 flex flex-col gap-6 mx-auto">
+        <div id='faq'>
+            <div className='max-w-[1140px] mx-auto xl:pb-[89px] lg:pb-14 pb-12'>
+            <CustomHeading title="Faqs"/>
+            </div>
+            <div className="border-t-2 border-b-2 border-black">
+                <div className="max-w-[1140px] flex flex-col border-r-2 border-l-2 border-black mx-auto">
                     {FAQS_LIST.map((item, index) => (
                         <div
                             key={index}
-                            className="rounded-xl p-6 border border-solid"
+                            className={`pt-8 pb-[43px] pl-[30px] pr-11 ${active === index ? '!pb-6' : ''} border-b-2 border-black ${index === 6 ? '!border-b-0' : ''}`}
                         >
-                            <button onClick={() => toggle(index)} className='flex w-full text-black justify-between max-sm:gap-4 bg-transparent items-center text-left max-sm:text-base font-medium text-2xl max-md:text-lg' >
+                            <button onClick={() => toggle(index)} className='cursor-pointer flex w-full text-black justify-between max-sm:gap-4 bg-transparent items-center text-left max-sm:text-base font-medium text-2xl max-md:text-lg' >
                                 {item}{" "}
-                                <span>
-                                    {active === index ? "+" : "-"}
+                                <span className='text-2xl font-semibold'>
+                                    {active === index ? "-" : "+"}
                                 </span>
                             </button>
-                            <p className={`text-black max-md:text-sm overflow-hidden overflow-y-auto transition-all duration-300 ${active === index ? "max-h-32 max-sm:max-h-52 pt-4" : "max-h-0" }`} >
-                                "hello"
+                            <p className={`text-black max-md:text-sm overflow-hidden max-[992px]:overflow-y-auto transition-all duration-300 max-w-[992px] ${active === index ? "max-h-32 max-sm:max-h-52 pt-4" : "max-h-0" }`} >
+                                NFT stands for “Non-fungible token,” which means that it’s a unique, digital item with blockchain-managed ownership that users can buy, own, and trade. Some NFT’s fundamental function is to be digital art. But they can also offer additional benefits like exclusive access to websites, event tickets, game items, and ownership records for physical objects. Think of it as a unique piece of art that can also work as a “members-only” card. Hustlin' Hardos works like this
                             </p>
                         </div>
                     ))}
